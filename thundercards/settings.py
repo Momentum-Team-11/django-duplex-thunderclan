8ce0bcf06b58a5d7c9c7524d1ce6ba6d78ef4510
KarenElaine <karenelaine.stewart@yahoo.com>
2022-03-11 15:40:19 -0500

    fixed database setting

--- thundercards/settings.py
+++ thundercards/settings.py
@@ -91,11 +91,10 @@ WSGI_APPLICATION = 'thundercards.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
+  
         'default': env.db(),
-    }
 }
+
 
 #CACHES = {
     # Read os.environ['CACHE_URL'] and raises
