2cc6a9632eebb24a7f4ea6a8828153f285226185
KarenElaine <karenelaine.stewart@yahoo.com>
2022-03-12 18:44:24 -0500

    added URL Patterns and notes :)

--- thundercards/urls.py
+++ thundercards/urls.py
@@ -18,9 +18,27 @@ from django.urls import path
 from django.conf import settings
 from django.urls import include, path
 
-#testing
+from . import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.show_deck, name='show_deck'), #shows all of the decks
+    #line 27 action URL for form will be decks/<int:pk>/cards
+    path("decks/<int:pk>/cards", card_views.add_card, name="add_card"), #allows user to add a new card to the deck
+    #line 29 will require a show_deck.html template
+    path("decks/<int:pk>/", cards_views.show_deck, name="show_deck"), #shows all of the cards in the deck
+    path(
+        "decks/<int:pk>/cards/<int:card_pk>/edit/", #shows which deck the card belongs to 
+        cards_views.edit_contact,
+        name="edit_card",
+    ),
+    path(
+        "decks/<int:pk>/cards/<int:pk>/delete/", 
+        cards_views.delete_card,
+        name="delete_card",
+    ),
+
 ]
 
 if settings.DEBUG:
