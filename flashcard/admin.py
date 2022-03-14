6620ec066e6afa48ea6d660573b6302b26c89702
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-14 18:41:33 -0400

    added new models to django admin so we can input data

--- flashcard/admin.py
+++ flashcard/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Deck, Card
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Deck)
+admin.site.register(Card)
